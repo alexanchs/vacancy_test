fd08c23c498e8bad02d93ab3dbee2430b44ff0a3
alex_ukrainskyy <ukrainskyy@gmail.com>
2023-09-02 12:19:28 +0300

    Initial commit

---
+++ src/main/java/com/example/vacancy_test/VacancyTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.vacancy_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vacancy_test/VacancyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vacancy_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
